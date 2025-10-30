1c4545ec2957234f48f215a07e3a0a41c6679bf1
mohammedsalihc <mohammedsalihmac@gmail.com>
2025-10-30 13:01:34 +0530

    added shubha

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,8 @@ app.get("/", (_, res) => {
   res.send("🚀 Hello from TypeScript + Docker + EC2!");
 });
 
+app.get('/subha',(req,res)=>{
+    res.send("Hello from Subha");
+})
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
