13418ecb91eceaabf057f2ad659a2f23fc028763
mohammedsalihc <mohammedsalihmac@gmail.com>
2025-10-30 15:19:15 +0530

    bug fix

--- src/index.ts
+++ src/index.ts
@@ -15,4 +15,8 @@ app.get('/salih',(req,res)=>{
     res.send("hello from  salih")
 })
 
+app.get('/amal',(req,res)=>{
+    res.send("hello from amal")
+})
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
